037e08c3a9938128fcf71f481fb9b46118336963
robertobuccina <77169589+robertobuccina@users.noreply.github.com>
2021-04-07 21:51:21 +0200

    ebd

---
+++ passwordgenerator.js
@@ -0,0 +1,9 @@
+
+//inserisci nome
+var Name = prompt ("Inserisci il tuo nome");
+//inserisci cognome
+var Surname = prompt ("Inserisci il tuo cognome");
+//inserisci colore preferito
+var Color = prompt ("Inserisci il tuo colore preferito");
+//risultato
+document.getElementById("Credentials").innerHTML = "La tua Password è: NameSurnameColor21";
